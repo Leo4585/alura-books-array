a3c9b69cd63b7cd50fc4b57e522729a829512691
leonardo <vieiraleo10@hotmail.com>
2025-01-24 07:47:27 -0300

    Alteração no no filtro dos livros

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -3,10 +3,10 @@ const botoes = document.querySelectorAll('.btn');
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 
 function filtrarLivros() {
-  const elementosBtn = document.getElementById(this.id); // Captura id do elemento clicado
-  const categoria = elementosBtn.value;                  // Captura o valor da chave "value"
+  const elementoBtn = document.getElementById(this.id); // Captura id do elemento clicado
+  const categoria = elementoBtn.value;                  // Captura o valor da chave "value"
   let livrosFiltrados = livros.filter(livro => {
     return livro.categoria == categoria;
   });
-  console.table(livrosFiltrados)
+  exibirLivrosNaTela(livrosFiltrados);
 }
