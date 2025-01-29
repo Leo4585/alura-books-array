e1036a637d460511c8e9940dbfbfa80b18f7b3ff
leonardo <vieiraleo10@hotmail.com>
2025-01-29 07:25:00 -0300

    Inclusão do operador ternário

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,8 +5,6 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id); // Captura id do elemento clicado
   const categoria = elementoBtn.value;                  // Captura o valor da chave "value"
-  let livrosFiltrados = livros.filter(livro => {
-    return livro.categoria == categoria;
-  });
+  let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
   exibirLivrosNaTela(livrosFiltrados);
 }
