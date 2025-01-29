835de92ca14f4aebccf9a1852f70f0d2ad960190
leonardo <vieiraleo10@hotmail.com>
2025-01-29 06:15:20 -0300

    Modificações pontuais e inclusão do método sort para ordenar os livros por preço

--- app/main.js
+++ app/main.js
@@ -1,10 +1,11 @@
-let livros = [];
+let livros = [];  // Variável declarada no escopo global para poder ser acessada pelos outros documentos .js
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
-getBuscarLivrosDaAPI();
+getBuscarLivrosDaAPI();  // Aqui a chamada da função assíncrona atualizou a variável "livros"
 
 async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointDaAPI);
-  livros = await res.json();
+  livros = await res.json();                       // *** Aqui a variável "livros" é atualizada. É esta lista "livros" que é usada nos outros documentos .js
   let livrosComDesconto = aplicarDesconto(livros);
   exibirLivrosNaTela(livrosComDesconto);
 }
+
---
+++ app/metodoSort.js
@@ -0,0 +1,10 @@
+let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+
+btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco() {
+  let livrosOrdenados = livros.sort((a, b) => {
+    return a.preco - b.preco
+  });
+  exibirLivrosNaTela(livrosOrdenados);
+}
