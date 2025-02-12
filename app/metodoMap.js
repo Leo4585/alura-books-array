fc6ef8cce22ec71a787bc2a1c431dd26d68e588a
leonardo <vieiraleo10@hotmail.com>
2025-02-12 06:29:36 -0300

    Inclusão de comentários

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,7 +1,7 @@
 function aplicarDesconto(livros) {
   const desconto = 0.3;
   livrosComDesconto = livros.map(livro => {
-    return {...livro, preco: livro.preco - (livro.preco * desconto)}
+    return {...livro, preco: livro.preco - (livro.preco * desconto)} // Estrutura simplificada que retorna todo o array 'livros', mas alterando apenas o valor da chave 'preço'
   });
   return livrosComDesconto;
 }
